a1897c6f3576e97841b78ebec3e973135d673d4c
kr0ks <discordyy0@gmail.com>
2023-01-15 20:01:41 -0300

    feat: validate auth endpoint

--- middleware/auth/status.go
+++ middleware/auth/status.go
@@ -1,17 +1,16 @@
 package auth
 
 import (
-	"github.com/floppahost/backend/jwt"
+	"github.com/floppahost/backend/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Status(c *fiber.Ctx) error {
-	header := c.GetReqHeaders()
-	token := header["Authorization"]
-	_, err := jwt.Validar(token)
+	token := c.Cookies("token")
+	userClaims := database.VerifyUser(token)
 
-	if err != nil {
-		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": false, "message": "Not authorized", "auth": false})
+	if !userClaims.ValidUser || userClaims.Blacklisted {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": true, "message": "not authorized", "auth": false})
 	}
 
 	return c.Status(fiber.StatusAccepted).JSON(fiber.Map{"error": false, "message": "Authenticated", "auth": true})
