aa7942ce80bc7a9bf7eb0ebc97302ca33b49ff0b
kr0ks <discordyy0@gmail.com>
2023-01-17 12:13:41 -0300

    fix: purge invites route

--- middleware/admin/route.go
+++ middleware/admin/route.go
@@ -8,7 +8,7 @@ func Routes(router fiber.Router) {
 	group := router.Group("/")
 
 	group.Post("/wave", InviteWave)
-	group.Post("/invites/puurge", PurgeInvites)
+	group.Post("/invites/purge", PurgeInvites)
 	group.Post("/invite/user", GenerateInvite)
 	group.Post("/blacklist", BlacklistUser)
 	group.Post("/unblacklist", UnblacklistUser)
