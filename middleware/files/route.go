56bd07ce9b7553588502a78f54af0dd31e7308af
kr0ks <discordyy0@gmail.com>
2023-01-18 14:23:48 -0300

    feat: change on embed route

--- middleware/files/route.go
+++ middleware/files/route.go
@@ -10,5 +10,5 @@ func Routes(router fiber.Router) {
 	group.Put("/", Upload)
 	group.Get("/render/:id", Render)
 	group.Delete("/:file", DeleteFile)
-	group.Get("/render/embed", Render)
+	group.Get("/embed", Render)
 }
