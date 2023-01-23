a701293a189888895afff22cf52fb2a0271bc43e
Kr0ks <discordyy0@gmail.com>
2023-01-23 01:00:38 -0300

    feat: 10mb body limit

--- configs/fiber.go
+++ configs/fiber.go
@@ -8,7 +8,7 @@ import (
 var FiberApp = fiber.New(fiber.Config{
 
 	// we are using a diferent json encoder to boost performace; read: docs.gofiber.io/guide/faster-fiber
-
+	BodyLimit: 	   10 * 1024 * 1024,
 	JSONEncoder:   json.Marshal,
 	JSONDecoder:   json.Unmarshal,
 	Prefork:       true, // read: docs.gofiber.io/api/fiber
